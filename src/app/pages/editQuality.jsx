304f0312472feea00f6ec9b5ceba9e927c061dc2
Imran-72 <petrow318@gmail.com>
2021-12-22 12:18:17 +0300

    сделал put запрос и обновил данные

--- src/app/pages/editQuality.jsx
+++ src/app/pages/editQuality.jsx
@@ -1,12 +1,32 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import EditForm from "../components/ui/editForm";
 
 const EditQualityPage = () => {
-    return (
-        <>
-            <h1>Edit Quality Page</h1> <EditForm />
-        </>
-    );
+  const [quality, setQuality] = useState(null);
+  const id = useParams().id;
+  const qualityEndPoint = `http://localhost:4000/api/v1/quality/${id}`;
+  useEffect(async () => {
+    const { data } = await axios.get(qualityEndPoint);
+    setQuality(data.content);
+  }, []);
+
+  const handeleSubmit = (data) => {
+    axios
+      .put(qualityEndPoint, data)
+      .then((res) => console.log(res.data.content));
+  };
+  return (
+    <>
+      <h1>Edit Quality Page</h1>
+      {quality !== null ? (
+        <EditForm data={quality} onSubmit={handeleSubmit} />
+      ) : (
+        "Loading..."
+      )}
+    </>
+  );
 };
 
 export default EditQualityPage;
