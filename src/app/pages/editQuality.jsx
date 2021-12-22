50b294348e46a069ad21d8de4b0e8feac63c67d4
Imran-72 <petrow318@gmail.com>
2021-12-22 14:19:32 +0300

    Глобальное отлавливание неожиданных ошибок

--- src/app/pages/editQuality.jsx
+++ src/app/pages/editQuality.jsx
@@ -2,33 +2,38 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import EditForm from "../components/ui/editForm";
+axios.interceptors.response.use(
+  (res) => res,
+  function (error) {
+    const expextedErrors =
+      error.response &&
+      error.response.status >= 400 &&
+      error.response.status < 500;
+    if (!expextedErrors) {
+      console.log("Unexpected error");
+    }
+    return Promise.reject(error);
+  }
+);
 
 const EditQualityPage = () => {
   const [quality, setQuality] = useState(null);
   const id = useParams().id;
   const qualityEndPoint = `http://localhost:4000/api/v1/quality/${id}`;
-  useEffect(async () => {
-    const { data } = await axios.get(qualityEndPoint);
-    setQuality(data.content);
-  }, []);
-
   const handeleSubmit = async (data) => {
     try {
       await axios
         .put(qualityEndPoint, data)
         .then((res) => console.log(res.data.content));
     } catch (error) {
-      const expextedErrors =
-        error.response &&
-        error.response.status >= 400 &&
-        error.response.status < 500;
-      if (!expextedErrors) {
-        console.log("Unexpected error");
-      } else {
-        console.log("Expected error");
-      }
+      console.log("Expected error");
     }
   };
+
+  useEffect(async () => {
+    const { data } = await axios.get(qualityEndPoint);
+    setQuality(data.content);
+  }, []);
 
   return (
     <>
